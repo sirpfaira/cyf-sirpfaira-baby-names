9f2e25fa7d21261fe89b90c4f4f3c739f90b9ffc
Simba Pfaira <spfaira@yahoo.com>
2021-06-29 23:24:23 +0200

    Level 3 complete

--- src/App.js
+++ src/App.js
@@ -3,40 +3,77 @@ import React, { useState } from 'react';
 import babyNames from './data/babyNamesData.json';
 
 function App() {
-  const [displayArray, setDisplayArray] = useState(babyNames);
+  const [allNames, setAllNames] = useState(babyNames);
+  const [favNames, setFavNames] = useState([]);
 
-  const updateDisplay = (event) => {
+  const updateAllNames = (event) => {
     let word = event.target.value;
 
     if (word) {
-      setDisplayArray(
+      setAllNames(
         babyNames.filter((baby) =>
           baby.name.toUpperCase().includes(word.toUpperCase())
         )
       );
     } else {
-      setDisplayArray(babyNames);
+      setAllNames(babyNames);
     }
   };
 
-  return (
-    <div className='container'>
-      <div className='search-div'>
-        <input
-          className='search-box form-control'
-          type='text'
-          placeholder='Search baby name'
-          onChange={updateDisplay}
-        />
-      </div>
+  const moveToFavs = (event) => {
+    const id = event.target.id;
+    if (id > -1 && id < allNames.length) {
+      let tempArr = [...allNames];
+      const removedEl = tempArr.splice(id, 1);
+      setFavNames((preState) => preState.concat(removedEl));
+      setAllNames(tempArr);
+    }
+  };
 
-      <div className='all-div'>
+  const moveToAll = (event) => {
+    const id = event.target.id;
+    if (id > -1 && id < favNames.length) {
+      let tempArr = [...favNames];
+      const removedEl = tempArr.splice(id, 1);
+      setAllNames((preState) => preState.concat(removedEl));
+      setFavNames(tempArr);
+    }
+  };
+
+  const FavouriteList = () => {
+    return (
+      <div className='favs-div'>
         <ul className='names-ul'>
-          {displayArray
+          <p className='fav-label'>Favorites: </p>
+          {favNames
             .sort((baby1, baby2) => (baby1.name > baby2.name ? 1 : -1))
             .map((baby, index) => (
               <li
                 key={index}
+                id={index}
+                className={baby.sex === 'm' ? 'boy-name' : 'girl-name'}
+                baby={baby}
+                onClick={moveToAll}
+              >
+                {baby.name}
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const AllNames = () => {
+    return (
+      <div className='all-div'>
+        <ul className='names-ul'>
+          {allNames
+            .sort((baby1, baby2) => (baby1.name > baby2.name ? 1 : -1))
+            .map((baby, index) => (
+              <li
+                key={index}
+                onClick={moveToFavs}
+                id={index}
                 className={baby.sex === 'm' ? 'boy-name' : 'girl-name'}
               >
                 {baby.name}
@@ -44,6 +81,21 @@ function App() {
             ))}
         </ul>
       </div>
+    );
+  };
+  return (
+    <div className='container'>
+      <div className='search-div'>
+        <input
+          className='search-box form-control'
+          type='text'
+          placeholder='Search baby name'
+          onChange={updateAllNames}
+        />
+      </div>
+      <FavouriteList />
+      <hr />
+      <AllNames />
     </div>
   );
 }
